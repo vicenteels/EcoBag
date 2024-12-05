9689c6d1c18b36931ce04ae7454c55e0ff73576a
vicenteels <seganfredov@gmail.com>
2024-12-05 17:14:11 -0300

    .

--- ecobag/static/js/loading.js
+++ ecobag/static/js/loading.js
@@ -1,37 +1,3 @@
-function showLoading(){
-    const div = document.createElement("div");
-    div.classList.add("loading");
-    div.style.position = "fixed";
-    div.style.top = "0";
-    div.style.left = "0";
-    div.style.width = "100%";
-    div.style.height = "100%";
-    div.style.backgroundColor = "rgba(0, 0, 0, 0.5)";
-    div.style.display = "flex";
-    div.style.justifyContent = "center";
-    div.style.alignItems = "center";
-    div.style.fontSize = "24px";
-    div.style.color = "#fff";
-  
-    const label = document.createElement("label");
-    label.innerText = "Carregando...";
-  
-    div.appendChild(label);
-  
-    document.body.appendChild(div);
-  }
-
-  function hideLoading(){
-    const loadings = document.getElementsByClassName('loading');
-    if (loadings.length){
-      const loading = loadings[0];
-      while (loading.firstChild) {
-        loading.removeChild(loading.firstChild);
-      }
-      loading.remove();
-    }
-  }
-
 function cadastrar(){
     window.location.href = "../cadastro"
 }
